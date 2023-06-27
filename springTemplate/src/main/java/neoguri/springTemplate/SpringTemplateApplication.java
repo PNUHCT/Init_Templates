7bf37294a6b4a263b0a66a771f91cc14cf845489
neoguri <nyong9221@gmail.com>
2023-06-27 15:53:16 +0900

    :seedling: init : initialize

---
+++ springTemplate/src/main/java/neoguri/springTemplate/SpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package neoguri.springTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplateApplication.class, args);
+	}
+
+}
